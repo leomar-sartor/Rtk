cfad9af61a6d8efc6dabe01e97f0f8711760f835
Leomar Sartor <leomar_sartor@unochapeco.edu.br>
2022-05-08 20:17:17 -0300

    Teste com primeiro parametro da função - gtime_ts t

---
+++ Aplicacao/RtkApp/RtkApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RtkApp
+{
+    class Program
+    {
+        
+        [DllImport("C:\\Users\\Cliente\\Desktop\\RTKPOST\\Fonte\\src\\postpos.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int postpos(gtime_t ts);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===  INICIANDO APLICAÇÃO  ===");
+
+            var time = new gtime_t();
+            time.time = 12;
+            time.sec = 345;
+
+            int retorno = postpos(time);
+
+            Console.WriteLine("Retorno : " + retorno);
+
+            Console.WriteLine("=== FINALIZANDO APLICAÇÃO ===");
+            Console.ReadKey();
+        }
+
+        struct gtime_t
+        {
+            public long time { get; set; }
+            public double sec { get; set; }
+        }
+    }
+}
